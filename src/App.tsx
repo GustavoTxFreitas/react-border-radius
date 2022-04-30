62eb6f3f843bae985a95b46c8b36f39422f36a68
GustavoTxFreitas <gustavotxfreitas@protonmail.com>
2022-04-30 11:33:44 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+        <h1>Hello Vite + React!</h1>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+
+        
+    </div>
+  )
+}
